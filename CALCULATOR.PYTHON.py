ac4e50e217308425e4ad426801bd9f235f4ee386
Vatturu-Deepthi <141556373+Vatturu-Deepthi@users.noreply.github.com>
2023-08-16 20:15:14 +0530

    Update CALCULATOR.p

--- CALCULATOR.PYTHON.py
+++ CALCULATOR.PYTHON.py
@@ -1,31 +1,52 @@
-print('two numbers below')
-a=int(input('enter the first number:'))
-b=int(input('enter the second number:'))
-ch=0
-while ch<5:
-    print('calculator menu')
-    print('1.add')
-    print('2.subtract')
-    print('3.multiply')
-    print('4.divide')
-    print('5.exit')
-    ch=int(input('enter ur choice:'))
-    if ch==1:
-       c=a+b
-       print('sum:',c)
-    elif ch==2:
-       c=a-b
-       print('difference:,c')
-    elif ch==3:
-       c=a*b
-       print('product:,c')
-    elif ch==4:
-       c=a/b
-       print('quotient:,c')
-    elif ch==5:
-       break
-    else:
-       print('INVALID CHOICE')
+import tkinter as tk
+
+def calculate():
+    num1 = float(entry_num1.get())
+    num2 = float(entry_num2.get())
+    operation = operation_var.get()
+
+    if operation == "+":
+        result.set(num1 + num2)
+    elif operation == "-":
+        result.set(num1 - num2)
+    elif operation == "*":
+        result.set(num1 * num2)
+    elif operation == "/":
+        if num2 != 0:
+            result.set(num1 / num2)
+        else:
+            result.set("Cannot divide by zero")
+
+# Create the main window
+root = tk.Tk()
+root.title("Simple Calculator")
+
+# Create input fields
+entry_num1 = tk.Entry(root)
+entry_num2 = tk.Entry(root)
+operation_var = tk.StringVar()
+operation_choices = ["+", "-", "*", "/"]
+operation_var.set("+")  # Default operation choice
+
+# Create operation choice dropdown
+operation_menu = tk.OptionMenu(root, operation_var, *operation_choices)
+
+# Create a button to calculate the result
+calculate_button = tk.Button(root, text="Calculate", command=calculate)
+
+# Display the result
+result = tk.StringVar()
+result_label = tk.Label(root, textvariable=result)
+
+# Place widgets on the window
+entry_num1.pack()
+operation_menu.pack()
+entry_num2.pack()
+calculate_button.pack()
+result_label.pack()
+
+# Start the main loop
+root.mainloop()
         
     
     
